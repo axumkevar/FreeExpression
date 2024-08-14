375e1a5836a2bc7d17517f185c977e42650213bb
Axum Kevar <90418301+axumkevar@users.noreply.github.com>
2024-08-14 01:03:02 -0700

    Create machine.h
    create machine config file for debugging

---
+++ src/machine.h
@@ -0,0 +1,23 @@
+/*
+ * machine.h
+ *
+ * This file is part of FreeExpression.
+ *
+ * https://github.com/thetazzbot/FreeExpression
+ *
+ * FreeExpression is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2.
+ *
+ * FreeExpression is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FreeExpression. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+// If Using The Expression, Uncomment the Following
+#define MACHINE_EXPRESION
+// For Cake, Uncomment this line instead.
+//#define MACHINE_CAKE
